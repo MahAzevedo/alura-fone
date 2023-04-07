7aa97a72d8abba2abfd871882968f0e201462921
Mah <madavetto@gmail.com>
2023-04-07 14:17:31 -0300

    update

--- alura-fone/alura-fone.js
+++ alura-fone/alura-fone.js
@@ -1,13 +1,7 @@
 document.querySelector('input[type=tel]');
 
-//console.log('testando');
-
-//document.querySelector('input[type=tel]');
-
-//let input 
-
-//let inputType = document.querySelector('input[type=tel]');
-
-let mensagem = "ola mundo!";
+function teclado () {
+    alert("teclado")
+}
 
 
