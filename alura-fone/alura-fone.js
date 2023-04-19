24be04658a2ed01354a42dbc6341d0ad7021b23f
Mah <madavetto@gmail.com>
2023-04-19 20:39:57 -0300

    update

--- alura-fone/alura-fone.js
+++ alura-fone/alura-fone.js
@@ -4,6 +4,18 @@ const listaDeTeclas = document.querySelectorAll('input=button');
 
 console.log(listaDeTeclas);
 
+const listaDeTeclas = document.querySelectorAll('input[type=button]');
+const inputTel = document.querySelector('input[type=tel]');
+
+for (indice = 0; indice < listaDeTeclas.length; indice ++) {
+  const tecla = listaDeTeclas[indice];
+  
+  tecla.onClick = function () {
+    inputTel.value = inputTel.value + tecla.value;
+  }
+
+}
+
 
 
 
